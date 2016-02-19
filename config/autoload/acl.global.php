d75454e8eb2ab82404950f537acdeb66a2f11996
antoine-humbert <antoine.humbert@neoxia.com>
2016-02-19 10:05:55 +0100

    Clean Up Layout master

--- config/autoload/acl.global.php
+++ config/autoload/acl.global.php
@@ -1,5 +1,4 @@
 <?php
-// http://p0l0.binware.org/index.php/2012/02/18/zend-framework-2-authentication-acl-using-eventmanager/
 // First I created an extra config for ACL (could be also in module.config.php, but I prefer to have it in a separated file)
 return array(
     'acl' => array(
